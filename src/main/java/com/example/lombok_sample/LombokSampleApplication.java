39db0b7eae91436a472f6fe87af1997de7ef6c84
Ryo Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2025-03-23 19:04:17 +0900

    first commit

---
+++ src/main/java/com/example/lombok_sample/LombokSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.lombok_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LombokSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LombokSampleApplication.class, args);
+	}
+
+}
